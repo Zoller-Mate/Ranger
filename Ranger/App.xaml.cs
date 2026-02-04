f72666a100a366232d4a596fe32a5a2d1426b223
Zoller-Mate <zoller.mate@gmail.com>
2026-02-04 12:15:09 +0100

    Add project files.

---
+++ Ranger/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ranger
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
